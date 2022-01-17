0df04efc31bc36c8a74fe365e062b88fa7f068a9
JongdukJung <67305265+JongdukJung@users.noreply.github.com>
2022-01-17 19:25:22 +0900

    Create selectpolygon.js

---
+++ selectpolygon.js
@@ -0,0 +1,37 @@
+var xmlhttp
+
+function showInfo(str){
+    xmlhttp=GetXmlHttpObject();
+    if (xmlhttp==null){
+    alert ("Your browser does not support AJAX!");
+    return;
+}
+var url="http://koreanplant.info/forest/getInfo.php";
+url=url+"?q="+str;
+url=url+"&sid="+Math.random();
+xmlhttp.onreadystatechange=stateChanged;
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+}
+
+function stateChanged()
+{
+if (xmlhttp.readyState==4)
+  {
+  document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+  }
+}
+
+function GetXmlHttpObject(){
+if (window.XMLHttpRequest)
+  {
+  // code for IE7+, Firefox, Chrome, Opera, Safari
+  return new XMLHttpRequest();
+  }
+if (window.ActiveXObject)
+  {
+  // code for IE6, IE5
+  return new ActiveXObject("Microsoft.XMLHTTP");
+  }
+return null;
+}
