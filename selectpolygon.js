ee5611a74c34c0d800fad8b1df6e763d75a78841
JongdukJung <67305265+JongdukJung@users.noreply.github.com>
2022-01-18 09:39:33 +0900

    Update selectpolygon.js

--- selectpolygon.js
+++ selectpolygon.js
@@ -1,37 +1,14 @@
-var xmlhttp
-
 function showInfo(str){
-    xmlhttp=GetXmlHttpObject();
-    if (xmlhttp==null){
-    alert ("Your browser does not support AJAX!");
-    return;
-}
 var url="http://koreanplant.info/forest/getInfo.php";
 url=url+"?q="+str;
-//url=url+"&sid="+Math.random();
-xmlhttp.onreadystatechange=stateChanged;
-xmlhttp.open("GET",url,true);
-xmlhttp.send(null);
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("POST", url, true);
+xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xmlhttp.onreadystatechange = function() { 
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+    }
 }
-
-function stateChanged()
-{
-if (xmlhttp.readyState==4)
-  {
-  document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
-  }
-}
-
-function GetXmlHttpObject(){
-if (window.XMLHttpRequest)
-  {
-  // code for IE7+, Firefox, Chrome, Opera, Safari
-  return new XMLHttpRequest();
-  }
-if (window.ActiveXObject)
-  {
-  // code for IE6, IE5
-  return new ActiveXObject("Microsoft.XMLHTTP");
-  }
-return null;
+//xmlhttp.send("foo=bar&lorem=ipsum");
+// xmlhttp.send(new Int8Array());
+ xmlhttp.send();
 }
